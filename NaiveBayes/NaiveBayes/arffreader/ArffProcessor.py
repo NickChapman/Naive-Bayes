6e4ad933557005f3263de391cc4ad858a20b5465
NickChapman <nlc35@georgetown.edu>
2016-02-18 01:39:03 -0500

    Entropy calculations function done, split calculations function done, function to find where the splits should actually happen is done as well.

--- NaiveBayes/NaiveBayes/arffreader/ArffProcessor.py
+++ NaiveBayes/NaiveBayes/arffreader/ArffProcessor.py
@@ -1,7 +1,7 @@
-import random.choice
+import random.choice, math
 import utils
 
-class ArffFile(object):
+class ArffProcessor(object):
     """Loads and manages an ARFF file"""
     
     def __init__(self, file_path):
@@ -146,10 +146,118 @@ class ArffFile(object):
                 # something has definitely gone wrong in that case
                 raise NotImplementedError("Need to implement handling for types beyond categorical and numeric")
 
+    def entropy_discretize_numerics(self, core_attribute, gain_threshold=.1):
+        """ Converts all numerical attributes to categorical
+        Uses entropy based discretization. 
+        """
+        self.map_attributes_to_num()
+        for attribute in self.attributes:
+            # We are only concerned with numeric attributes
+            if attribute[1].lower() == "numeric":
+                attr_name = attribute[0]
+                # Sort the data into ascending order based on that attribute
+                self.data.sort(key=lambda x : x[self.attr_position[attr_name]])
+                attr_ranges = []
+                self.get_splits(0, len(self.data), attr_name, core_attribute, gain_threshold, attr_ranges)
+                # TODO: CONTINUE HERE
+                
+    def get_splits(self, lower_index, upper_index, binning_attribute, core_attribute, gain_threshold, ranges):
+        """ RANGES WILL CONTAIN ALL OF THE FINAL RANGE VALUES"""
+        results = self.find_best_split(lower_index, upper_index, binning_attribute, core_attribute, gain_threshold)
+        should_split = results[0]
+        lower_range = results[1]
+        upper_range = results[2]
+        if not should_split:
+            ranges.append((lower_index, upper_index))
+        else:
+            # Find the index where the split occurs
+            split_value = lower_range[1]
+            split_index = lower_index
+            for i in range(lower_index + 1, upper_index + 1):
+                if self.data[i][self.attr_position[binning_attribute]] >= split_value:
+                    break
+                else:
+                    split_index += 1
+            self.get_splits(lower_index, split_index, binning_attribute, core_attribute, gain_threshold, ranges)
+            self.get_splits(split_index + 1, upper_index, binning_attribute, core_attribute, gain_threshold, ranges)
+                    
+    def find_best_split(self, lower_index, upper_index, binning_attribute, core_attribute, gain_threshold):
+        """ ASSUMES DATA IS SORTED ON BINNING_ATTRIBUTE
+        @returns a tuple of one bool and 2 range tuples: (should_split, (min_value, ideal_split), (ideal_split, max_value))
+        """
+        # Get the bins starting entropy
+        # TODO: Correct the following assumption
+        # Assume that core_attributes are always categorical
+        overall_entropy = self.entropy(lower_index, upper_index, float("inf"), binning_attribute, core_attribute)
+        # best split is initially the first split
+        ideal_split = (self.data[lower_index][self.attr_position[binning_attribute]] 
+                       + self.data[lower_index + 1][self.attr_position[binning_attribute]]) / 2
+        ideal_entropy = self.entropy(lower_index, upper_index, ideal_split, binning_attribute, core_attribute)
+        # Calculate the entropy for every possible split
+        for i in range(lower_index + 1, upper_index):
+            split = (self.data[i][self.attr_position[binning_attribute]] 
+                       + self.data[i + 1][self.attr_position[binning_attribute]]) / 2
+            split_entropy = self.entropy(lower_index, upper_index, split, binning_attribute, core_attribute)
+            if split_entropy < ideal_entropy:
+                ideal_split = split
+                ideal_entropy = split_entropy
+        # Determine whether it is worth it to split up this range
+        should_split = False
+        if (overall_entropy - ideal_entropy) >= gain_threshold:
+            should_split = True
+        range_min = self.data[lower_index][self.attr_position[binning_attribute]]
+        range_max = self.data[upper_index][self.attr_position[binning_attribute]]
+        return (should_split, (range_min, ideal_split), (ideal_split, range_max))
+
+    def entropy(self, lower_index, upper_index, split_point, binning_attribute, core_attribute):
+        """ ASSUMES DATA IS SORTED ON BINNING_ATTRIBUTE """
+        sample_size = upper_index - lower_index + 1;
+        probabilities = {}
+        net_entropy = 0;
+        lower_entropy = 0
+        upper_entropy = 0
+        lower_bin_size = 0
+        upper_bin_size = 0
+        # Get entropy for the bin less than split_point
+        for attr_value in self.attributes[self.attr_position[core_attribute]]:
+            # Ensuring that none of the probabilities come out to 0 ensures the entropy calculation works
+            probabilities[attr_value] = .5
+        # Count the occurences of each core attribute value in the lower range
+        for i in range(lower_index, upper_index + 1):
+            if self.data[i][self.attr_position[binning_attribute]] < split_point:
+                probabilities[self.data[i][self.attr_position[core_attribute]]] += 1
+                lower_bin_size += 1
+        # Perform the actual entropy calculation
+        for attr_value in probabilities:
+            p = probabilities[attr_value] / lower_bin_size
+            lower_entropy += p * math.log2(p)
+        # Multiply the result by negative 1 to factor in the fact that it is -Sum...
+        lower_entropy *= -1
+
+        # Repeat for the upper bin
+        # Get entropy for the bin greater than or equal to split_point
+        for attr_value in self.attributes[self.attr_position[core_attribute]]:
+            # Ensuring that none of the probabilities come out to 0 ensures the entropy calculation works
+            probabilities[attr_value] = .5
+        # Count the occurences of each core attribute value in the lower range
+        for i in range(lower_index, upper_index + 1):
+            if self.data[i][self.attr_position[binning_attribute]] >= split_point:
+                probabilities[self.data[i][self.attr_position[core_attribute]]] += 1
+                upper_bin_size += 1
+        # Perform the actual entropy calculation
+        for attr_value in probabilities:
+            p = probabilities[attr_value] / upper
+            upper_entropy += p * math.log2(p)
+        # Multiply the result by negative 1 to factor in the fact that it is -Sum...
+        upper_entropy *= -1.0
+
+        # Calculate the net entropy
+        net_entropy = lower_bin_size / sample_size * lower_entropy + upper_bin_size / sample_size * upper_entropy
+        return net_entropy
 
     def map_attributes_to_num(self):
         """Maps the attribute to its position in a data line"""
         self.attr_position = {}
         for i, attribute in enumerate(self.attributes):
             attr_name = attribute[0]
-            self.attr_position[attribute_name] = i    
+            self.attr_position[attribute_name] = i
--- NaiveBayes/NaiveBayes/utils.py
+++ NaiveBayes/NaiveBayes/utils.py
@@ -8,3 +8,28 @@ def get_dict_modes(dict):
         if dict[key] == highest_count:
             modes.append(key)
     return modes
+
+class NumericalDataBin():
+    def __init__(self, **kwargs):
+        """
+        @param gte: kwarg for the minimum value it must be greater than or equal to
+        @param lt: kwarg for the maximum value it must be less than
+        """
+        self.min = kwargs.get("gte", -float("inf"))
+        self.max = kwargs.get("lt", float("inf"))
+
+    def belongs_to_bin(self, item):
+        if not isinstance(item, int) or not isinstance(item, float):
+            return False
+        if item > self.max or item < self.min:
+            return False
+        return True
+
+    def add_item(self, item):
+        if not isinstance(item, int) or not isinstance(item, float):
+            raise TypeError("NumericalDataBin is for numbers only")
+        if item > self.max or item < self.min:
+            raise ValueError("An item was added to the bin that is beyond the scope of the bin")
+        self.values.append(item)
+
+    
